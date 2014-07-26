2a164fb04265c0942edacfd1a950add0daa16103
Frank Carnevale <frank@fourward.org>
2014-07-26 15:58:16 -0400

    Set goal to active upon creation

--- models/goal.rb
+++ models/goal.rb
@@ -2,6 +2,6 @@ class Goal < ActiveRecord::Base
   belongs_to :user
 
   def self.add_goal(user, content)
-    user.goals.create(content: content)
+    user.goals.create(content: content, active: true)
   end
 end
