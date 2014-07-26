46495078b5af5afb94984aa56eeda468a4ead8dd
Frank Carnevale <frank@fourward.org>
2014-07-26 12:52:38 -0400

    Remove root path route

--- app.rb
+++ app.rb
@@ -4,9 +4,6 @@ require './config/environments'
 require './models/user'
 require 'twilio-ruby'
  
-get '/' do
-    "Hello, World!"
-end
 
 get '/gitterdone' do
   twiml = Twilio::TwiML::Response.new do |r|
