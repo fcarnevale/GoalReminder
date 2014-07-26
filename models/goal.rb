dc43928002e63a7e4284c0959f9f785d952ca039
Frank Carnevale <frank@fourward.org>
2014-07-26 19:07:33 -0400

    Still trying to find active goal bug

--- models/goal.rb
+++ models/goal.rb
@@ -2,13 +2,9 @@ require 'active_record'
 
 class Goal < ActiveRecord::Base
   belongs_to :user
-  attr_accessor :active
 
   def self.add_goal(user, content)
-    goal = user.goals.create(content: content)
-    
-    goal.active = true
-    goal.save
+    goal = user.goals.create(content: content, active: true)
   end
 
   def self.active_goals_for(user)
