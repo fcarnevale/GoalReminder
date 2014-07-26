8712ccc8963b046894b6098fe5ec6d9a78a6a81c
Frank Carnevale <frank@fourward.org>
2014-07-25 23:21:26 -0400

    Add a test message on /gitterdone URL

--- app.rb
+++ app.rb
@@ -7,3 +7,10 @@ require 'twilio-ruby'
 get '/' do
     "Hello, World!"
 end
+
+get '/gitterdone' do
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message "Hallo! Testing this bad boy out!"
+  end
+  twiml.text
+end
