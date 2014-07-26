8f4d0fff0910f7a25e9ca260207e5e2c2cee935f
Frank Carnevale <frank@fourward.org>
2014-07-26 13:29:56 -0400

    Simplify high level logic in message handling

--- app.rb
+++ app.rb
@@ -10,15 +10,9 @@ get '/gitterdone' do
   twiml = Twilio::TwiML::Response.new do |r|
     phone_number = params[:From]
     message = params[:Body]
-
     user = IncomingSMS.find_or_create_user(phone_number)
 
-    if user.created_today
-      r.Message "Hi! Text a goal for the week like so 'Goal yourgoalhere'"
-      r.Message IncomingSMS.handle_message(message) if message
-    else
-      r.Message IncomingSMS.handle_message(message) if message
-    end
+    r.Message IncomingSMS.handle_message(message) if message
   end
   twiml.text
 end
