432e67ab938c9a84b1cdc93bb97de9a5a6c78995
Frank Carnevale <frank@fourward.org>
2014-07-26 13:47:09 -0400

    Testing another method
    - Also remove explicit return from help! method

--- lib/incoming_sms.rb
+++ lib/incoming_sms.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../models/user', __FILE__)
 
 class IncomingSMS
-  ALLOWABLE_USER_METHODS = [:help!]
+  ALLOWABLE_USER_METHODS = [:help!, :test]
 
   class << self  
     def find_or_create_user(phone_number)
@@ -25,7 +25,11 @@ class IncomingSMS
     end
 
     def help!
-      return "Text your goals for the week, one by one, like so 'Goal yourgoalhere'"
+      "Text your goals for the week, one by one, like so 'Goal yourgoalhere'"
+    end
+
+    def test(content)
+      "Testing metaprogramming. Your message: #{content}."
     end
   end
 end
