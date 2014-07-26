e086d12dfe409a90fc53611f844af0410f2e2f38
Frank Carnevale <frank@fourward.org>
2014-07-26 16:15:34 -0400

    Add basic exception handling notification

--- goal_reminder.rb
+++ goal_reminder.rb
@@ -2,29 +2,44 @@ require './models/user'
 require './models/goal'
 require 'twilio-ruby'
 
-db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/goal_reminder_development')
- 
-ActiveRecord::Base.establish_connection(
-  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
+begin
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/goal_reminder_development')
+   
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
 
-@account_sid = ENV['TWILIO_SID']
-@auth_token = ENV['TWILIO_AUTH_TOKEN'] 
-@from = ENV['TWILIO_PHONE_NUMBER']
+  @account_sid = ENV['TWILIO_SID']
+  @auth_token = ENV['TWILIO_AUTH_TOKEN'] 
+  @from = ENV['TWILIO_PHONE_NUMBER']
 
-@client = Twilio::REST::Client.new(@account_sid, @auth_token)
-@account = @client.account 
+  @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+  @account = @client.account 
 
-Goal.where(active: true).find_each do |goal|
-  message = "Goal reminder: #{goal.content}"
+  Goal.where(active: true).find_each do |goal|
+    message = "Goal reminder: #{goal.content}"
+    @account.messages.create(
+      :from => @from, 
+      :to => goal.user.mobile_phone,
+      :body => message
+    )
+  end
+rescue Exception => e
   @account.messages.create(
     :from => @from, 
-    :to => goal.user.mobile_phone,
-    :body => message
+    :to => @from,
+    :body => "Error running goal reminder script: #{e}."
+  )
+ensure
+  @account.messages.create(
+    :from => @from, 
+    :to => @from,
+    :body => "Finished running goal reminder script."
   )
 end
+  
