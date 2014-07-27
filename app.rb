a35699be300ece02abb2778c0a72572b35a6fbf6
Frank Carnevale <frank@fourward.org>
2014-07-26 20:21:12 -0400

    Add some protection against someone visiting /gitterdone directly
    without any SMS-related params

--- app.rb
+++ app.rb
@@ -7,12 +7,16 @@ require './lib/incoming_sms'
 require 'pry'
 
 get '/gitterdone' do
-  twiml = Twilio::TwiML::Response.new do |r|
-    phone_number = params[:From]
-    message = params[:Body]
-    user = IncomingSMS.find_or_create_user(phone_number) unless phone_number.blank?
+  if params
+    twiml = Twilio::TwiML::Response.new do |r|
+      phone_number = params[:From]
+      message = params[:Body]
+      user = IncomingSMS.find_or_create_user(phone_number) unless phone_number.blank?
 
-    r.Message IncomingSMS.handle_message(user, message) if message
+      r.Message IncomingSMS.handle_message(user, message) if message
+    end
+    twiml.text
+  else
+    "Gitterdone!"
   end
-  twiml.text
 end
