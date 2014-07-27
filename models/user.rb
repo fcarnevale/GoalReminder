ad7fc32bef357e3bcd821cc919d8526f16c24bf4
Frank Carnevale <frank@fourward.org>
2014-07-26 20:22:58 -0400

    Validate mobile phone format in User model

--- models/user.rb
+++ models/user.rb
@@ -3,6 +3,9 @@ require 'active_record'
 class User < ActiveRecord::Base
   has_many :goals
 
+  MOBILE_PHONE_REGEX = /\A\+\d{11}\z/
+  validates_format_of :mobile_phone, with: MOBILE_PHONE_REGEX, on: :create
+
   def active_goals
     goals.where(active: true)
   end
