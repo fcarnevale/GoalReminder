9b83872b88a6f16edcdf2ec77c89cb54f0b3d995
Frank Carnevale <frank@fourward.org>
2014-07-31 22:38:52 -0400

    Missing a bunch of params, but just found twilio's own validator

--- app.rb
+++ app.rb
@@ -23,20 +23,40 @@ get '/gitterdone' do
 end
 
 get '/testingauth' do
-  request_uri = request.env['REQUEST_URI']
-  hashed_request = Digest::HMAC.hexdigest(request_uri, ENV['TWILIO_AUTH_TOKEN'], Digest::SHA1)
-  encoded_request = Base64.encode64(hashed_request)
-  authorized = encoded_request == request.env['HTTP_X_TWILIO_SIGNATURE']
-  twilio_sig = request.env['HTTP_X_TWILIO_SIGNATURE']
-  debug_text = "request_uri: #{request_uri}, encoded_request: #{encoded_request}, header: #{twilio_sig}"
+  # request_uri = request.env['REQUEST_URI']
+  # hashed_request = Digest::HMAC.hexdigest(request_uri, ENV['TWILIO_AUTH_TOKEN'], Digest::SHA1)
+  # encoded_request = Base64.encode64(hashed_request)
+  # authorized = encoded_request == request.env['HTTP_X_TWILIO_SIGNATURE']
+  # twilio_sig = request.env['HTTP_X_TWILIO_SIGNATURE']
+  # debug_text = "request_uri: #{request_uri}, encoded_request: #{encoded_request}, header: #{twilio_sig}"
+
+  # First, instantiate a RequestValidator object with your account's AuthToken.
+  validator = Twilio::Util::RequestValidator.new(ENV['TWILIO_AUTH_TOKEN'])
+
+  # Then gather the data required to validate the request. The following works in
+  # sinatra, and something similar should work in any rack-based environment.
+
+  # Build the URI for this request, including query string params if any.
+  uri = request.original_url
+
+  # Collect all parameters passed from Twilio.
+  params = env['rack.request.form_hash']
+  # If GET, use rack.request.query_hash instead:
+  # params = env['rack.request.query_hash']
+
+  # Grab the signature from the HTTP header.
+  signature = env['HTTP_X_TWILIO_SIGNATURE']
+
+  # Finally, call the validator's #validate method.
+  authorized = validator.validate uri, params, signature #=> true if the request is from Twilio
 
   if authorized
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Authorized request! #{debug_text}"
+      r.Message "Authorized request!"
     end
   else
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Unauthorized request! #{debug_text}"
+      r.Message "Unauthorized request!"
     end
   end
   
